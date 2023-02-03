e384d1b2d39181d89ccb412149bdb826be0ed88a
Clayton Siby <claytonsiby@gmail.com>
2023-02-03 10:04:07 +0200

    Conditional state updates with mapStateToProps and mapDispatchToProps

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -4,6 +4,7 @@ import CakeContainer from './components/CakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 import './App.css';
 
 function App() {
@@ -11,12 +12,14 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <ItemContainer />
         <h3>*************** HOOKS IN ACTION **************</h3>
         <HooksCakeContainer />
         <h3>+++++++++++++++ IceCreams ++++++++++++++++++++</h3>
         <IceCreamContainer />
         <h3>=============== Action Payload ===============</h3>
         <NewCakeContainer />
+        <ItemContainer />
       </div>
     </Provider>
   );
