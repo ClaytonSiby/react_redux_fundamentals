ede86f7a90964e16c15414cb8de983cfb11dedcf
Clayton Siby <claytonsiby@gmail.com>
2023-02-03 07:48:54 +0200

    Add: react-redux hooks to access and update the store

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 import './App.css';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <h3>*************** HOOKS IN ACTION **************</h3>
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
